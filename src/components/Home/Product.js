36239157d4dc55747b395651912219bc3f0ee3a1
Stephan <sgriesel@gmail.com>
2020-07-11 17:03:50 +0200

    test

--- src/components/Home/Product.js
+++ src/components/Home/Product.js
@@ -1,23 +1,23 @@
-import React from 'react'
-import Img from 'gatsby-image'
+// import React from 'react'
+// import Img from 'gatsby-image'
 
-export default function Product({ product }) {
-    return (
-        <div className="col-10 col-sm-8 col-md-6 col-lg-4 mx-auto my-3">
-            <div className="card" style={{ minHeight: "100%" }}>
-                <Img fluid={product.image.fluid} className="card-img-top" />
-                <div className="card-body text-center">
-                    <h6>{product.title}</h6>
-                    <h6>R{product.price}</h6>
-                    <button
-                        className="btn btn-green mt-3 text-capitalize snipcart-add-item"
-                        data-item-id={product.id}
-                        data-item-name={product.title}
-                        data-item-price={product.price}
-                        data-item-image={product.image.fluid.src}
-                        data-item-url="https://happyskin.netlify.app"
-                    >Add</button>
-                </div>
-            </div>
-        </div>);
-}
+// export default function Product({ product }) {
+//     return (
+//         <div className="col-10 col-sm-8 col-md-6 col-lg-4 mx-auto my-3">
+//             <div className="card" style={{ minHeight: "100%" }}>
+//                 <Img fluid={product.image.fluid} className="card-img-top" />
+//                 <div className="card-body text-center">
+//                     <h6>{product.title}</h6>
+//                     <h6>R{product.price}</h6>
+//                     <button
+//                         className="btn btn-green mt-3 text-capitalize snipcart-add-item"
+//                         data-item-id={product.id}
+//                         data-item-name={product.title}
+//                         data-item-price={product.price}
+//                         data-item-image={product.image.fluid.src}
+//                         data-item-url="https://happyskin.netlify.app"
+//                     >Add</button>
+//                 </div>
+//             </div>
+//         </div>);
+// }
