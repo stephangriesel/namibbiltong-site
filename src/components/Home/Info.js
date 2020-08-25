0d2c2b0eb2bc5b225f32cbac340f0284456116c2
Stephan <sgriesel@gmail.com>
2020-08-25 19:25:12 +0200

    typo fix & styles update

--- src/components/Home/Info.js
+++ src/components/Home/Info.js
@@ -14,7 +14,7 @@ export default function Info() {
                             <p>Namib Biltong started in 2002 in the Western Cape and later expanded to Gauteng and the rest of Africa.
                             It is a family-owned company that supplies meat and biltong to major retail outlets in South Africa and Africa.
                             Deliveries are done extensively throughout South Africa with our fleet of merchandisers and representatives.
-We have done extensive research and are market leaders in pre-packed biltong development which ensures a longer shelf-life for our products.  All our facilities comply with all major food management syste</p>
+We have done extensive research and are market leaders in pre-packed biltong development which ensures a longer shelf-life for our products.  All our facilities comply with all major food management systems.</p>
                         </div>
                         {/* <Link to="/about">
                             <button className="btn text-uppercase btn-yellowish">About</button>
