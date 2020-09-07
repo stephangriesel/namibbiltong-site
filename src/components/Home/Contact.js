8ef238d83b1bd40265aee2283a07f2b3349104b4
Stephan <sgriesel@gmail.com>
2020-09-07 09:09:04 +0200

    update fields to required, no validation yet

--- src/components/Home/Contact.js
+++ src/components/Home/Contact.js
@@ -13,15 +13,15 @@ export default function Contact() {
                         method="POST">
                         <div className="form-group">
                             {/* <label htmlFor="name">Name</label> */}
-                            <input type="text" className="form-control" name="name" id="name" placeholder="Your Name" />
+                            <input type="text" className="form-control" name="name" id="name" placeholder="Your Name" required />
                         </div>
                         <div className="form-group">
                             {/* <label htmlFor="name">Name</label> */}
-                            <input type="text" className="form-control" name="email" id="email" placeholder="Your Email" />
+                            <input type="text" className="form-control" name="email" id="email" placeholder="Your Email" required />
                         </div>
                         <div className="form-group">
                             {/* <label htmlFor="name">Name</label> */}
-                            <textarea name="description" id="description" className="form-control" rows="3" placeholder="Tell us more.." />
+                            <textarea name="description" id="description" className="form-control" rows="3" placeholder="Tell us more.." required />
                         </div>
                         <button type="submit" className="btn btn-whiteish btn-block text-capitalize mt-5">Talk to us</button>
                     </form>
